947a96a5d43f03c3f8e8c163cabad775588d0a02
sesankm <sesank.mallikarjuna@gmail.com>
2025-01-14 18:04:28 -0600

    draw grid

--- main.c
+++ main.c
@@ -3,17 +3,34 @@
 
 #define WIN_W 1500
 #define WIN_H 900
+#define CELL_W 50
+#define NUM_ROWS WIN_H / CELL_W
+#define NUM_COLS WIN_W / CELL_W
+
+void drawGrid(SDL_Renderer* rend){
+	SDL_SetRenderDrawColor(rend, 0, 0, 0, 100);
+	for(int i = 0; i < WIN_H; i += CELL_W) {
+		SDL_RenderDrawLine(rend, 0, i, WIN_W, i);
+	}
+	for(int i = 0; i < WIN_W; i += CELL_W) {
+		SDL_RenderDrawLine(rend, i, 0, i, WIN_H);
+	}
+}
+
 
 void tick(SDL_Renderer* rend, int updateInteveralNanoSeconds){
 	SDL_SetRenderDrawColor(rend, 255, 255, 255, 255);
 	SDL_RenderClear(rend);
+
+	drawGrid(rend);
+
 	SDL_RenderPresent(rend);
 	usleep(updateInteveralNanoSeconds);
 }
 
 int main(){
 	if(SDL_Init(SDL_INIT_VIDEO) < 0) {
-		perror("Couldn't initialize SDL2");
+		perror("Couldn't initialize SDL2 video subsystem");
 		return -1;
 	}
 
