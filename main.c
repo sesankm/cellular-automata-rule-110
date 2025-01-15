2e95b302f143f2fd7161885afa919a427d151853
sesankm <sesank.mallikarjuna@gmail.com>
2025-01-14 23:23:18 -0600

    use array to determine next state; update colors

--- main.c
+++ main.c
@@ -3,13 +3,14 @@
 
 #define WIN_W 1500
 #define WIN_H 900
-#define CELL_W 10
+#define CELL_W 5 
 #define NUM_ROWS WIN_H / CELL_W
 #define NUM_COLS WIN_W / CELL_W
 
+
 void drawGrid(SDL_Renderer*);
 void drawCells(SDL_Renderer*, int**);
-void updateCells(int**);
+void updateCells(int**, int[]);
 void tick(SDL_Renderer*, int, int**);
 
 int main(){
@@ -18,7 +19,8 @@ int main(){
 		return -1;
 	}
 
-	SDL_Window* window = SDL_CreateWindow("VPaper", 200, 200, WIN_W, WIN_H, SDL_WINDOW_RESIZABLE);
+	int rule110[] = {0, 1, 1, 1, 0, 1, 1, 0};
+	SDL_Window* window = SDL_CreateWindow("VPaper", 200, 200, WIN_W, WIN_H, SDL_WINDOW_FULLSCREEN);
 	SDL_Renderer* rend = SDL_CreateRenderer(window, 0, SDL_RENDERER_SOFTWARE);
 
 	int running = 1;
@@ -30,16 +32,16 @@ int main(){
 		memset(cells[i], 0, sizeof(int) * NUM_COLS);
 	}
 
-	cells[1][1] = 1;
+	cells[NUM_ROWS-1][NUM_COLS-1] = 1;
+
+	int gridInd, gridInd2;
 
 	while(running) {
 		while(SDL_PollEvent(&event)) {
 			switch(event.type){
 			case SDL_MOUSEBUTTONDOWN:
-				int mouseX = event.button.x;
-				int mouseY = event.button.y;
-				int gridInd = mouseX / CELL_W;
-				int gridInd2 = mouseY / CELL_W;
+				gridInd = event.button.x / CELL_W;
+				gridInd2 = event.button.y / CELL_W;
 				cells[gridInd2][gridInd] = 1;
 				break;
 			case SDL_KEYDOWN:
@@ -49,7 +51,7 @@ int main(){
 			}
 		}
 		tick(rend, 35500, cells);
-		updateCells(cells);
+		updateCells(cells, rule110);
 	}
 
 	SDL_DestroyWindow(window);
@@ -58,7 +60,6 @@ int main(){
 }
 
 void drawGrid(SDL_Renderer* rend){
-	SDL_SetRenderDrawColor(rend, 0, 0, 0, 100);
 	for(int i = 0; i < WIN_H; i += CELL_W) {
 		SDL_RenderDrawLine(rend, 0, i, WIN_W, i);
 	}
@@ -68,7 +69,7 @@ void drawGrid(SDL_Renderer* rend){
 }
 
 void drawCells(SDL_Renderer* rend, int** cells){
-	SDL_SetRenderDrawColor(rend, 0, 0, 0, 100);
+	SDL_SetRenderDrawColor(rend, 244, 185, 66, 255);
 	for(int i = 0; i < NUM_ROWS; i++) {
 		for(int j = 0; j < NUM_COLS; j++) {
 			if(cells[i][j] == 1) {
@@ -79,56 +80,27 @@ void drawCells(SDL_Renderer* rend, int** cells){
 	}
 }
 
-/*
-111	110	011	010
-0	1	1	1
-
-
-101	100	001	000
-1	0	1	0
-*/
-void updateCells(int** cells){
-	for(int i = 1; i < NUM_ROWS; i++) {
-		for(int j = 0; j < NUM_COLS; j++) {
-			if(cells[i][j] == 0) {
-				if(cells[i][j - 1] == 1 && cells[i][j + 1] == 1) {
-					cells[i - 1][j] = 1;
-				}
-				else if(cells[i][j - 1] == 1 && cells[i][j + 1] == 0) {
-					cells[i - 1][j] = 0;
-				}
-				else if(cells[i][j - 1] == 0 && cells[i][j + 1] == 1) {
-					cells[i - 1][j] = 1;
-				}
-				else if(cells[i][j - 1] == 0 && cells[i][j + 1] == 0) {
-					cells[i - 1][j] = 0;
-				}
-			}
-			else if(cells[i][j] == 1) {
-				if(cells[i][j - 1] == 1 && cells[i][j + 1] == 1) {
-					cells[i - 1][j] = 0;
-				}
-				else if(cells[i][j - 1] == 1 && cells[i][j + 1] == 0) {
-					cells[i - 1][j] = 1;
-				}
-				else if(cells[i][j - 1] == 0 && cells[i][j + 1] == 1) {
-					cells[i - 1][j] = 1;
-				}
-				else if(cells[i][j - 1] == 0 && cells[i][j + 1] == 0) {
-					cells[i - 1][j] = 1;
-				}
-			}
-		}
-	}
+int convertBinaryToInt(const char *s) {
+	return (int) strtol(s, NULL, 2);
 }
 
+void updateCells(int** cells, int rule110[]){
+	char* buffer = malloc(sizeof(char) * 3);
+	for(int i = 1; i < NUM_ROWS; i++) {
+		for(int j = 1; j < NUM_COLS; j++) {
+			sprintf(buffer, "%d%d%d", cells[i][j - 1], cells[i][j], cells[i][j + 1]);
+			int ruleIndex = convertBinaryToInt(buffer);
+			cells[i - 1][j] = rule110[ruleIndex];
+		}
+	}
+	free(buffer);
+}
+
+
 void tick(SDL_Renderer* rend, int updateInteveralNanoSeconds, int** cells){
-	SDL_SetRenderDrawColor(rend, 255, 255, 255, 255);
+	SDL_SetRenderDrawColor(rend, 239, 242, 241, 255);
 	SDL_RenderClear(rend);
-
-	//drawGrid(rend);
 	drawCells(rend, cells);
-
 	SDL_RenderPresent(rend);
 	usleep(updateInteveralNanoSeconds);
 }
